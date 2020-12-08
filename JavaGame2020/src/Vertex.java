68e4cb203bb17fbc5c8ccc372e8ef4cf558a15b3
MarekCzernohous <MarekCzernohous@0-208-Lehrer>
2020-12-08 14:40:42 +0100

    Vertex commit

--- JavaGame2020/src/Vertex.java
+++ JavaGame2020/src/Vertex.java
@@ -1,8 +1,13 @@
 public class Vertex {
 
-	public static void main(String[] args) {
+	public double x;
+	public double y;
+
+	public Vertex(double x, double y) {
+
+		this.x=x;
+		this.y=y;
 		
-
+		
 	}
-
 }
